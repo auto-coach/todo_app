43a6d7ddbbb9ac4550171dc83a6262b06ad84030
A G <imhigaf@MacBook-Pro-A.local>
2022-06-13 16:58:51 +0300

    init

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  fixturesFolder: false,
+  reporter: "mochawesome",
+  reporterOptions: {
+    "reportDir": "cypress/report",
+    "charts": true,
+    "reportPageTitle": "todo test results",
+    "saveHtml": true,
+    "reportFilename": "index",
+    "overwrite": true,
+  },
+  "video": false,
+  e2e: {
+    setupNodeEvents(on, config) { },
+    baseUrl: 'http://localhost:8888',
+  },
+})
